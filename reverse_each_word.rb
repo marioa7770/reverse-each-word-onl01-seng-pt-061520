fe5b711c9691dd5db12fce27285e74c73e45e95a
mario acevedo <jun_acevedo415@yahoo.com>
2020-06-27 05:18:12 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,5 @@
 def reverse_each_word(array)
   new_array = array.split(" ")
-  reversed_array = new_array.each {|x| x.reverse!}
-  return reversed_array.join(" ")
-end
+  new_array.collect {|x| x.reverse!}
+  new_array.join(" ")
+endl
